7a2536531134fc28f1d7e00c0b275279fa062556
misteraioli <nicolasperez.catalan@gmail.com>
2024-10-23 16:36:26 +0200

    merci driss pour la review

--- src/instruc.c
+++ src/instruc.c
@@ -6,7 +6,7 @@
 /*   By: niperez <niperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 18:42:26 by niperez           #+#    #+#             */
-/*   Updated: 2024/09/24 16:26:03 by niperez          ###   ########.fr       */
+/*   Updated: 2024/10/23 16:33:44 by niperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,12 @@ static void	ft_instruc(const char *type, int i, char **name, char **instruc)
 
 void	ft_r(int i, t_list **lists, char **name, char **instruc)
 {
-	if (ft_lstsize(lists[i]) > 1)
+	if (ft_lstsize(lists[i]) == 2)
+	{
+		lst_swap(&lists[i]);
+		ft_instruc("s", i, name, instruc);
+	}
+	if (ft_lstsize(lists[i]) > 2)
 	{
 		lst_rotate(&lists[i]);
 		ft_instruc("r", i, name, instruc);
@@ -35,7 +40,12 @@ void	ft_r(int i, t_list **lists, char **name, char **instruc)
 
 void	ft_rr(t_list **lists, char **name, char **instruc)
 {
-	if (ft_lstsize(lists[0]) > 1)
+	if (ft_lstsize(lists[0]) == 2)
+	{
+		lst_swap(&lists[0]);
+		ft_instruc("s", 0, name, instruc);
+	}
+	if (ft_lstsize(lists[0]) > 2)
 	{
 		lst_reverse_rotate(&lists[0]);
 		ft_instruc("rr", 0, name, instruc);
